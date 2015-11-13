57e7dd947e7f89358952bdbbc78daba66af95e9a
Sean Craswell <scraswell@gmail.com>
2015-11-12 23:17:18 -0400

    Initial commit.

---
+++ AesEncryptionToolTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Craswell.Encryption.UnitTests
+{
+    [TestFixture()]
+    public class AesEncryptionToolTests
+    {
+        [Test()]
+        public void AesEncryptionTool_CanEncryptAndDecrypt()
+        {
+            const string teststring = "Hello world!";
+            const string passphrase = "The quick brown fox jumps over the lazy dog.";
+
+            AesEncryptionTool tool = new AesEncryptionTool();
+
+            string ciphertext = tool.EncryptText(teststring, passphrase);
+            Assert.AreNotEqual(teststring, ciphertext);
+
+            string decryptedText = tool.DecryptText(ciphertext, passphrase);
+            Assert.AreEqual(teststring, decryptedText);
+        }
+    }
+}
+
