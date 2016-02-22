e7f08341d32cf3375be39dfa34278057fd8ce57d
Sean Craswell <scraswell@gmail.com>
2016-02-21 21:11:28 -0400

    Added methods to encrypt integer data type.  Needs refactoring, more generic.

--- AesEncryptionToolTests.cs
+++ AesEncryptionToolTests.cs
@@ -7,7 +7,7 @@ namespace Craswell.Encryption.UnitTests
     public class AesEncryptionToolTests
     {
         [Test()]
-        public void AesEncryptionTool_CanEncryptAndDecrypt()
+        public void AesEncryptionTool_CanEncryptAndDecryptText()
         {
             const string teststring = "Hello world!";
             const string passphrase = "The quick brown fox jumps over the lazy dog.";
@@ -19,6 +19,21 @@ namespace Craswell.Encryption.UnitTests
 
             string decryptedText = tool.DecryptText(ciphertext, passphrase);
             Assert.AreEqual(teststring, decryptedText);
+        }
+
+        [Test()]
+        public void AesEncryptionTool_CanEncryptAndDecryptInteger()
+        {
+            const int testinteger = 42;
+            const string passphrase = "The quick brown fox jumps over the lazy dog.";
+
+            AesEncryptionTool tool = new AesEncryptionTool();
+
+            string ciphertext = tool.EncryptInt(testinteger, passphrase);
+            Assert.AreNotEqual(testinteger, ciphertext);
+
+            int decryptedInteger = tool.DecryptInt(ciphertext, passphrase);
+            Assert.AreEqual(decryptedInteger, testinteger);
         }
     }
 }
